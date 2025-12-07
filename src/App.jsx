a801efc7c9cf5c792aebb76978c594e79e6945d0
rahmanshaon <rahmanshaon45@gmail.com>
2025-12-08 02:36:03 +0600

    initial project setup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>RouteLynk</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-6">
+        <h1 className="text-4xl font-bold">
+          Welcome to <span className="text-gradient">RouteLynk</span>
+        </h1>
+      </div>
     </>
   );
 }
