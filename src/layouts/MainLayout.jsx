848e8cc2caf77159a1e0a4d3c2739865bb33ed39
rahmanshaon <rahmanshaon45@gmail.com>
2025-12-11 21:33:24 +0600

    navigation loading added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,38 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "../components/Shared/Navbar";
 import Footer from "../components/Shared/Footer";
+import Loader from "../components/Shared/Loader";
 
 const MainLayout = () => {
+  const location = useLocation();
+  const [navigating, setNavigating] = useState(false);
+
+  // --- Navigation Loading Logic ---
+  useEffect(() => {
+    setNavigating(true);
+
+    const timer = setTimeout(() => {
+      setNavigating(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
       <main className="flex-1">
-        <Outlet />
+        {navigating ? (
+          <Loader
+            fullScreen={false}
+            className="min-h-[60vh]"
+            message="Loading Page..."
+          />
+        ) : (
+          <Outlet />
+        )}
       </main>
 
       <Footer />
